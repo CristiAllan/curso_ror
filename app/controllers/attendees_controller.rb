f9cc89b15e1b0f9057644f735474ef6306320f5b
Cristi Allan <cristiallan@gmail.com>
2016-04-28 13:19:37 -0300

    Add simple form gem and save attendee form data

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -4,6 +4,21 @@ class AttendeesController < ApplicationController
     end
 
     def create
+        @attendee = Attendee.new(attendee_params)
 
+        if @attendee.save
+            redirect_to root_path
+        else
+            render :new
+        end
+
+
+
+    end
+
+    private
+
+    def attendee_params
+        params.require(:attendee).permit(:name, :email, :cpf, :phone)
     end
 end
--- spec/controllers/attendees_controller_spec.rb
+++ spec/controllers/attendees_controller_spec.rb
@@ -13,4 +13,48 @@ RSpec.describe AttendeesController, type: :controller do
         end
 
     end
+
+    describe 'POST #create' do
+        context 'with valid data' do
+            let(:params) do
+                {"name"=>"Cristi", "email"=>"cristi@teste.com", "cpf"=>"06442783669", "phone"=>"987030798"}
+            end
+
+            it 'create a new attendee' do
+                expect do
+                    post :create, attendee: params
+                end.to change(Attendee, :count).from(0).to(1)
+
+                attendee = Attendee.last
+                expect(attendee.name).to eq("Cristi")
+                expect(attendee.cpf).to eq("06442783669")
+                expect(attendee.email).to eq("cristi@teste.com")
+                expect(attendee.phone).to eq("987030798")
+            end
+
+
+            it 'redirect to root path' do
+                post :create, attendee: params
+                expect(response).to redirect_to(root_path)
+            end
+        end
+
+        context 'with invalid data' do
+            let(:params) do
+                {"name"=>"Cristi", "email"=>"", "cpf"=>"06442783669", }
+            end
+
+            it ' do not create a new attendee' do
+                expect do
+                    post :create, attendee: params
+                end.to_not change(Attendee, :count).from(0)
+            end
+
+
+            it 'render form' do
+                post :create, attendee: params
+                expect(response).to render_template(:new)
+            end
+        end
+    end
 end
